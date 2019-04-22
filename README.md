6935983a2c6887e19ef2ada1b8f0db4ce958c2ae
gyape <gyape@192.168.0.6>
2019-04-22 15:59:54 +0200

    Design, UI, Requirements, Project Map
    Added first version of the desgin file.

--- README.md
+++ README.md
@@ -1,17 +1,32 @@
-# NetTrafficStat
+# NetTrafficStat project
+Introduction
 - This project is created to have a simple application which makes monthly/weekly statistics about the data traffic on a computer
  - The motivation is to be sure that the data limitations of available wireless internet services(eg. 30GB, 60GB, 150GB telenor hipernet home+s ... home+l) are enough to work from a farm where wireless internet services are available.
 
--- Functional requirements:
+
+ System modules
+ - UI: user interactions to schedule data traffic acquisition and display statistic  
+ - StatisticCollector: runs, stops data traffic acquisition on configured ethernet ports.
+
+ Third-party modules
+ - Logger
+ - Pcap4J
+ - Native library for packet capture(eg. WinPcap, libpcap)
+ - JavaFX
+
+ Functional requirements
  - The application shall collect data traffic from selectable ethernet ports. Eg eth1, eth2 or all.
  - The application shall create statistics about the upload and download traffic also for each selected eth and their sum also.
  - The application shall have an automatic run at OS startup option.
+ - The application shall create log entries about events, operations for debugging released or under development application
  
--- System modules:
- - UI: user interactions to schedule data traffic acquisition and display statistic  
- - StatisticCollector: runs, stops data traffic acquisition on configured ethernet ports.
- - Logger: logging events, operations for debugging released application
+ Module requirements
+ - StatisticCollector:
+     - Module shall store the current amount of data(measured from the start of the acquisition).  
 
 # Project Map
-folders:
-- install: contains the installer for WinPcap. official website is https://www.winpcap.org/
+Folders
+- doc: contains documentation about the system(eg. design)
+- install: contains the installer of WinPcap. official website is https://www.winpcap.org/
+- src/main/java: contains all the source files regarding this application
+- target: contains the deployable or runnable application
--- src/main/java/NetTrafficStatApp.java
+++ src/main/java/NetTrafficStatApp.java
@@ -3,43 +3,64 @@
  */
 package com.github.gyapeee;
 
+/*
+ * Importing stuffs for DevDunegonExample
+ * */
 import java.io.IOException;
+import org.omg.CORBA.portable.ApplicationException;
 import org.pcap4j.core.PcapNetworkInterface;
 import org.pcap4j.util.NifSelector;
+
+/*
+ * Importing stuffs for JavaFX GUI tutorial
+ * */
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
 /**
  * @author gyape
  *
  */
-public class NetTrafficStatApp {
+public class NetTrafficStatApp extends Application{
 
+	private void doTheDevDungeonExample() {
+		/* Some example code from DevDungeon tutorial 
+		 * to be sure that the Pcap4J integrated successfully:
+		 * https://www.devdungeon.com/content/packet-capturing-java-pcap4j
+		 * */
+				
+		        // The class that will store the network device
+		        // we want to use for capturing.
+		        PcapNetworkInterface device = null;
+
+		        // Pcap4j comes with a convenient method for listing
+		        // and choosing a network interface from the terminal
+		        try {
+		            // List the network devices available with a prompt
+		            device = new NifSelector().selectNetworkInterface();
+		        } catch (IOException e) {
+		            e.printStackTrace();
+		        }
+
+		        System.out.println("You chose: " + device);
+		/*
+		 * End of the DevDungeon example
+		 * */     
+	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// This is the Entry point of the Application
-		
-/* Some example code from DevDungeon tutorial 
- * to be sure that the Pcap4J integrated successfully:
- * https://www.devdungeon.com/content/packet-capturing-java-pcap4j
- * */
-		
-        // The class that will store the network device
-        // we want to use for capturing.
-        PcapNetworkInterface device = null;
-
-        // Pcap4j comes with a convenient method for listing
-        // and choosing a network interface from the terminal
-        try {
-            // List the network devices available with a prompt
-            device = new NifSelector().selectNetworkInterface();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("You chose: " + device);
-/*
- * End of the DevDungeon example
- * */        
+		launch(args);
+   
 
 	}
 
